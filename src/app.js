d72183ea5c6ad8f041c4fc3abaafb070a39aefd2
Falk Kühnel <falk@kuehnel.eu>
2022-04-30 14:45:38 +0200

    we have game root webpage

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send("<html><head>"+
+        "<script src=\"https://code.jquery.com/jquery-3.6.0.min.js\"   integrity=\"sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=\" crossorigin=\"anonymous\"></script>"+
+        "<script src=\"/game.js\"></script>" +
+        "<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"> \n" +
+        "<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin> \n" +
+        "<link href=\"https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=VT323&display=swap\" rel=\"stylesheet\">"+
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/game.css\" />"+
+        "</head><body><h1>Hello Gamers</h1><div id=\"gameInfo\"><p>Welcome to our new Adventure!<br>" +
+        "</p></div><form id=\"commandForm\" ><input id=\"command\" type=\"text\"/></form>"+
+        "</body></html>")
+})
+
+app.get('/command', (req, res) => {
+    res.send("WTF");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
