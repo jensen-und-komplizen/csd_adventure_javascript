aba2ee04dc703ff4636e3b9cd0a7570556554cec
Stefan Buchholtz <stefan.buchholtz@proximity.de>
2022-05-03 15:33:41 +0200

    new tests for new loo commands

--- test/unittests/Loo.test.js
+++ test/unittests/Loo.test.js
@@ -4,3 +4,15 @@ test('Loo to have a detailed description with a dollar', () => {
     loo = new Loo()
     expect(loo.getDetailedDescription()).toContain('dollar');
 });
+
+test('Look at playboy should work', () => {
+    loo = new Loo();
+    const response = loo.tell("look at playboy")
+    expect(response).toContain('rather not.');
+});
+
+test('Look at nasty jokes should work', () => {
+    loo = new Loo();
+    const response = loo.tell("read nasty joke")
+    expect(response).toContain('the toaster say');
+});
