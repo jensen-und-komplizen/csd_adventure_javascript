aaf2bbff894fd2cba11140f79c4fba420a4a0cdd
Kai Sißmeir <kai.sissmeir@interone.de>
2022-05-05 14:42:32 +0200

    feat(prettier) format code

--- src/Adventure.js
+++ src/Adventure.js
@@ -1,77 +1,80 @@
-const Loo = require("./Rooms/Loo");
-const Help = require("./public/Help");
-const Player = require("./Characters/Player");
-const WashRoom = require("./Rooms/WashRoom");
+const Loo = require('./Rooms/Loo')
+const Help = require('./public/Help')
+const Player = require('./Characters/Player')
+const WashRoom = require('./Rooms/WashRoom')
 
 class Adventure {
-  #loo;
-  #washRoom;
-  #help;
-  #currentRoom;
-  lastResponse;
-  #counter = 0;
-  player;
+    #loo
+    #washRoom
+    #help
+    #currentRoom
+    lastResponse
+    #counter = 0
+    player
 
-  start() {
-    this.player = new Player();
-    this.#loo = new Loo(this.player);
-    this.#washRoom = new WashRoom();
-    this.#help = new Help();
-    this.#currentRoom = this.#loo;
-    this.lastResponse = this.#currentRoom.getDescription();
-    return this.lastResponse;
-  }
-
-  tell(command) {
-    let response;
-    let originalCommand = command;
-    console.log(command);
-    command = command.toLowerCase();
-
-    switch (command) {
-      case 'help':
-      case "":
-        response = this.#help.getHelpText()
-        break
-      case "observe":
-      case "scout":
-      case "scout around":
-      case "look":
-      case "look around":
-        response = this.#currentRoom.getDetailedDescription();
-        break;
-      case "washroom":
-      case "enter the washroom":
-      case "go to the washroom":
-      case "go to washroom":
-      case "go through door":
-      case "enter door":
-      case "enter the door":
-      case "door":
-      case "go washroom":
-      case "move to washroom":
-      case "enter washroom":
-        this.#currentRoom = this.#washRoom;
-        response = "You enter the washroom.";
-        break;
-      case "count":
-        this.#counter++;
-        response = "The counter is at " + this.#counter;
-        break;
-      default:
-        response = this.#currentRoom.tell(command);
-        if (!response) {
-          response = "I don't understand " + originalCommand + ", you can try 'help' for some support!";
-        }
-        break;
+    start() {
+        this.player = new Player()
+        this.#loo = new Loo(this.player)
+        this.#washRoom = new WashRoom()
+        this.#help = new Help()
+        this.#currentRoom = this.#loo
+        this.lastResponse = this.#currentRoom.getDescription()
+        return this.lastResponse
     }
-    this.lastResponse = response;
-    return response;
-  }
 
-  getCurrentRoom() {
-    return this.#currentRoom;
-  }
+    tell(command) {
+        let response
+        let originalCommand = command
+        console.log(command)
+        command = command.toLowerCase()
+
+        switch (command) {
+            case 'help':
+            case '':
+                response = this.#help.getHelpText()
+                break
+            case 'observe':
+            case 'scout':
+            case 'scout around':
+            case 'look':
+            case 'look around':
+                response = this.#currentRoom.getDetailedDescription()
+                break
+            case 'washroom':
+            case 'enter the washroom':
+            case 'go to the washroom':
+            case 'go to washroom':
+            case 'go through door':
+            case 'enter door':
+            case 'enter the door':
+            case 'door':
+            case 'go washroom':
+            case 'move to washroom':
+            case 'enter washroom':
+                this.#currentRoom = this.#washRoom
+                response = 'You enter the washroom.'
+                break
+            case 'count':
+                this.#counter++
+                response = 'The counter is at ' + this.#counter
+                break
+            default:
+                response = this.#currentRoom.tell(command)
+                if (!response) {
+                    response =
+                        "I don't understand " +
+                        originalCommand +
+                        ", you can try 'help' for some support!"
+                }
+                break
+        }
+        this.lastResponse = response
+        return response
+    }
+
+    getCurrentRoom() {
+        return this.#currentRoom
+    }
 }
 
-module.exports = Adventure;
+module.exports = Adventure
--- src/public/Help.js
+++ src/public/Help.js
@@ -1,24 +1,23 @@
 class Help {
+    #help = []
 
-  #help = []
+    constructor() {
+        this.#help.push('Help! <br>')
+        this.#help.push('You can use the following commands. <br>')
+        this.#help.push('Commands: <br>')
+        this.#help.push('- look around <br>')
+        this.#help.push('Extended commands:')
+        this.#help.push('Replace [item] with an item you see or possess: <br>')
+        this.#help.push('- read [item]<br>')
+        this.#help.push('- look at [item]<br>')
+        this.#help.push('- pick up [item]<br>')
+        this.#help.push('- go to [room]<br>')
+        this.#help.push('- count<br>')
+    }
 
-  constructor() {
-    this.#help.push('Help! <br>')
-    this.#help.push('You can use the following commands. <br>')
-    this.#help.push('Commands: <br>')
-    this.#help.push('- look around <br>')
-    this.#help.push('Extended commands:')
-    this.#help.push('Replace [item] with an item you see or possess: <br>')
-    this.#help.push('- read [item]<br>')
-    this.#help.push('- look at [item]<br>')
-    this.#help.push('- pick up [item]<br>')
-    this.#help.push('- go to [room]<br>')
-    this.#help.push('- count<br>')
-  }
-
-  getHelpText() {
-    return this.#help;
-  }
+    getHelpText() {
+        return this.#help
+    }
 }
 
 module.exports = Help
