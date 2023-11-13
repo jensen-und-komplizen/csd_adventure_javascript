2363ea450ab6af2047cd81caa04334275dd00a6b
Team International <gunnar.quehl@interone.de>
2023-11-13 16:21:38 +0100

    soften jokes

--- src/Rooms/Loo.js
+++ src/Rooms/Loo.js
@@ -5,10 +5,10 @@ class Loo {
     #player
 	#jokes = [
 		'What did the toaster say to the sliced bread? I want you inside me!',
-		'Give it to me! Give it to me!" she yelled. I am so wet, give it to me now! She could scream all she wanted, but I was keeping the umbrella.',
+		`Chuck Noris doesn't ask ChatGPT, ChatGPT asks Chuck Noris`,
 		'They say that during sex you burn off as many calories as running eight miles. Who the hell runs eight miles in 30 seconds?',
 		'I will admit it, I have a tremendous sex drive. My girlfriend lives forty miles away.',
-		'What is the difference between kinky and perverted? Kinky is when you tickle your girlfriend with a feather, perverted is when you use the whole bird.',
+		'How do you call a collection of dad jokes? a DADabase',
 		'I bet you can not tell me something that will make me both happy and sad at the same time, a husband says to his wife. She thinks about it for a moment and then responds, Your penis is bigger than your brothers.',
 		'How do you make a pool table laugh? Tickle its balls.'
 	]
